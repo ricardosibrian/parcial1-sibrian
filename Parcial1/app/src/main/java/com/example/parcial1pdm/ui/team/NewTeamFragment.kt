fc878c9eb42a0af29c9d6ffaf115599750c04519
knella-system-app <82784367+knella-system-app@users.noreply.github.com>
2023-05-23 10:00:46 -0600

    Add: LiveData and DataBinding implementation

--- Parcial1/app/src/main/java/com/example/parcial1pdm/ui/team/NewTeamFragment.kt
+++ Parcial1/app/src/main/java/com/example/parcial1pdm/ui/team/NewTeamFragment.kt
@@ -11,53 +11,53 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.parcial1pdm.R
 import com.example.parcial1pdm.data.model.TeamModel
+import com.example.parcial1pdm.databinding.FragmentNewTeamBinding
 import com.google.android.material.textfield.TextInputEditText
 
 class NewTeamFragment : Fragment() {
+
+    private lateinit var binding: FragmentNewTeamBinding
 
     private val teamViewModel: TeamViewModel by activityViewModels{
         TeamViewModel.Factory
     }
 
-    lateinit var name: TextInputEditText
-    lateinit var sport: TextInputEditText
-    lateinit var actionSubmit: Button
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_team, container, false)
+        binding = FragmentNewTeamBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        bind(view)
+        setViewModel()
+        observeStatus()
 
-        actionSubmit.setOnClickListener {
-            createTeam()
         }
 
+    private fun setViewModel() {
+        binding.viewmodel = teamViewModel
     }
 
-    private fun bind(view: View){
-        name = view.findViewById(R.id.TextField1)
-        sport = view.findViewById(R.id.TextField2)
-        actionSubmit = view.findViewById(R.id.addTracker)
-    }
+    private fun observeStatus() {
+        teamViewModel.status.observe(viewLifecycleOwner) { status ->
+            when {
+                status.equals(TeamViewModel.WRONG_DATA) -> {
+                    Log.d("APP TAG", status)
+                    teamViewModel.clearStatus()
+                }
+                status.equals(TeamViewModel.TEAM_CREATED) -> {
+                    Log.d("APP TAG", status)
+                    Log.d("APP TAG", teamViewModel.getTeams().toString())
 
-    private fun createTeam(){
-        val newTeam = TeamModel(
-            name.text.toString(),
-            sport.text.toString(),
-        )
-        teamViewModel.addTeam(newTeam)
-
-        Log.d("APP TAG", teamViewModel.getTeams().toString())
-
-        findNavController().popBackStack()
+                    teamViewModel.clearStatus()
+                    findNavController().popBackStack()
+                }
+            }
+        }
     }
 
 
--- Parcial1/app/src/main/java/com/example/parcial1pdm/ui/team/TeamViewModel.kt
+++ Parcial1/app/src/main/java/com/example/parcial1pdm/ui/team/TeamViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.parcial1pdm.ui.team
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
@@ -10,8 +11,47 @@ import com.example.parcial1pdm.repositories.TeamRepository
 
 class TeamViewModel(private val repository: TeamRepository): ViewModel() {
 
+    // Aqui se implementa LiveData
+    var name = MutableLiveData("")
+    var sport = MutableLiveData("")
+    var status = MutableLiveData("")
+
     fun getTeams() = repository.getTeams()
     fun addTeam(team: TeamModel) = repository.addTeam(team)
+
+    fun createTeam() {
+        if (!validateData()) {
+            status.value = WRONG_DATA
+            return
+        }
+
+        val newTeam = TeamModel(
+            name.value!!,
+            sport.value!!,
+        )
+
+        addTeam(newTeam)
+        clearData()
+
+        status.value = TEAM_CREATED
+    }
+
+    private fun validateData(): Boolean {
+        when {
+            name.value.isNullOrEmpty() -> return false
+            sport.value.isNullOrEmpty() -> return false
+        }
+        return true
+    }
+
+    fun clearData() {
+        name.value = ""
+        sport.value = ""
+    }
+
+    fun clearStatus() {
+        status.value = INACTIVE
+    }
 
     companion object {
         val Factory = viewModelFactory {
@@ -20,6 +60,10 @@ class TeamViewModel(private val repository: TeamRepository): ViewModel() {
                 TeamViewModel(app.teamRepository)
             }
         }
+
+        const val TEAM_CREATED = "Team created"
+        const val WRONG_DATA = "Wrong data"
+        const val INACTIVE = ""
     }
 
 }
